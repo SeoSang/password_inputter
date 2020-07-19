b79239ba96d4f49fbff257dadc4a8e67c35cb618
SeoSang <ddrrpg@naver.com>
2020-07-20 01:55:16 +0900

    입력기 완성 |  세부정리, submit 구현 필요

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,8 @@ import Inputter from "./Inputter_flex"
 function App() {
   return (
     <div className='main'>
-      <p>
-        4자리 비밀번호 입력기 입니다. <br></br> 비밀번호를 입력해주세요
-      </p>
+      <h1>비밀번호 입력기</h1>
+      <p>비밀번호를 입력해주세요</p>
       <Inputter></Inputter>
     </div>
   )
--- src/Inputter_flex.tsx
+++ src/Inputter_flex.tsx
@@ -1,31 +1,86 @@
-import React, { useState } from "react"
+import React, { useState, MouseEvent, useCallback } from "react"
 import "./Inputter_flex.css"
 
 const shuffle = (nums: number[]) => {
+  // 배열 섞는 함수
   let num_length = nums.length
   while (num_length) {
+    console.log("here")
     let random_index = Math.floor(num_length-- * Math.random())
     let temp = nums[random_index]
     nums[random_index] = nums[num_length]
     nums[num_length] = temp
-    num_length -= 1
   }
-  return
+  return nums
 }
 
 const Inputter = () => {
   let nums_init = Array.from({ length: 10 }, (v, k) => k)
-  shuffle(nums_init)
   const [nums, setNums] = useState(nums_init)
+  const [password, setPassword] = useState("")
+
+  const handlePasswordChange = useCallback(
+    (num) => {
+      setPassword(password + num.toString())
+    },
+    [password],
+  )
+
+  const erasePasswordOne = useCallback(
+    (e: MouseEvent) => {
+      setPassword(password.slice(0, password.length == 0 ? 0 : password.length - 1))
+    },
+    [password],
+  )
+
+  const erasePasswordAll = useCallback(
+    (e: MouseEvent) => {
+      setPassword("")
+    },
+    [password],
+  )
+
+  const shuffleNums = useCallback(
+    (num: number) => (e: MouseEvent) => {
+      // 0 ~ 9 섞어주기
+      let nums_random = Array.from({ length: 10 }, (v, k) => k)
+      setNums(shuffle(nums_random))
+      handlePasswordChange(num)
+    },
+    [nums, password],
+  )
 
   return (
-    <div className='inputter__flex'>
-      {nums.map((n, i) => (
-        <button className='num-button__flex' value={n}>
-          {n}
+    <>
+      <input className='password-container' type='password' value={password}></input>
+      <div className='inputter__flex'>
+        {nums.map((n, i) => {
+          const Basic_button = (
+            <button
+              className='num-button__flex spread-effect'
+              value={n}
+              onClick={shuffleNums(n)}
+              key={i}
+            >
+              {n}
+            </button>
+          )
+          return i == nums.length - 1 ? (
+            <>
+              <button className='num-button__flex spread-effect' onClick={erasePasswordAll}>
+                X
+              </button>
+              {Basic_button}
+            </>
+          ) : (
+            Basic_button
+          )
+        })}
+        <button className='num-button__flex spread-effect' onClick={erasePasswordOne}>
+          ←
         </button>
-      ))}
-    </div>
+      </div>
+    </>
   )
 }
 
